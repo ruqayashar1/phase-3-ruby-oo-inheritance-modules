2beeb1f2727974378b4f25a4f76f5c5644270c65
rukia <ruqayashar1@gmail.com>
2023-05-26 07:35:54 +0300

    completed

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,16 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
+
+class Kid
+    include Dance
+    extend MetaDancing
+  attr_accessor :name
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  def initialize(name)
+    @name = name
+  end
+end
+
